e06f0c0ccf9b6c1c9991e4589aa271d4ec7dba87
Tristan Elliott <>
2022-07-27 14:44:44 -0300

    initial commit

---
+++ src/main/java/com/Java/DSA/demo/JavaDsaApplication.java
@@ -0,0 +1,13 @@
+package com.Java.DSA.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Java/DSA/demo/JavaDsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Java.DSA.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
