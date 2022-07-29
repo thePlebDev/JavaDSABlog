3c14954531c853ec008cfa31dbae1637795eb8ae
Tristan Elliott <>
2022-07-28 21:23:18 -0300

    blog post editor version 1

--- src/main/java/com/Java/DSA/demo/controllers/HomeController.java
+++ src/main/java/com/Java/DSA/demo/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.Java.DSA.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,5 +11,18 @@ public class HomeController {
     @GetMapping("/")
     public String home(){
         return "home";
+    }
+
+    @GetMapping("/post/trees/1")
+    public String treeOne(Model model){
+        model.addAttribute("test","<h1>another one</h1>");
+        return "blogPostOne";
+    }
+
+    @GetMapping("/admin/post/create")
+    public String createPost(Model model){
+        model.addAttribute("test","<h1 class=\"red\">testing</h1>");
+
+        return "createPost";
     }
 }
---
+++ src/main/resources/static/javascript/blogPostEditor.js
@@ -0,0 +1,10 @@
+//const textArea = document.getElementById('my_text_area');
+//const editDisplay = document.getElementById('editDisplay');
+//
+//textArea.addEventListener('input', () => {
+//
+//    editDisplay.innerHTML = textArea.value;
+//
+//})
+
+
